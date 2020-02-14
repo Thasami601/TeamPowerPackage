1cbcb4c577628f174717c4ed29bc9c0ef86c3368
Lebogang <lebogang.joyce@gmail.com>
2020-02-14 14:41:57 +0200

    Update Module.py
    Updated Funct 2 to add docstrings

--- Module.py
+++ Module.py
@@ -14,6 +14,22 @@ def dictionary_of_metrics(items):
 #FUNCTION 2
 
 def five_num_summary(items):
+      """ Calculates summary statistics of the given list :
+    maximum, minimum, median, quartile 1, quartile 3
+    
+    Parameters
+    ----------
+    items: A list of intergers/floats
+    
+    
+    Returns
+    -------
+    dict: 
+        Returns values of the dictionary as the summary staistics rounded
+        off to two decimal places and the description
+        of the values as keys.
+    
+    """
     # your code here
     a = 'max median min q1 q3'.split()
     b = [round(np.max(items),2),round(np.median(items),2),round(np.min(items),2),round(np.quantile(items,0.25),2),round(np.quantile(items, 0.75),2)]
