113152987cb0f763c7bb700ff0454916d2ef0b13
Simangaliso Mlangeni <simangalisosimphiwe08@gmail.com>
2020-02-18 16:01:41 +0200

    Update Module.py
    Correction on function 1

--- TeamPowerPackage/Module.py
+++ TeamPowerPackage/Module.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 
 def dictionary_of_metrics(items):
-
-    """ Calculates summary statistics of the given list :
+    """Calculates summary statistics of the given list :
     maximum, minimum, median, mean, variance, standard deviation 
     
     Parameters
@@ -18,12 +17,11 @@ def dictionary_of_metrics(items):
         Returns values of the dictionary as the summary staistics rounded
         off to two decimal places and the description
         of the values as keys."""    
-    for item in items:
-    # using numpy library to calculate mean, median, var and std        
-        return {'mean': round(np.mean(items), 2),  'median': round(np.median(items), 2),
-                'var': round(np.var(items, ddof = 1), 2), 
-                'std': round(np.std(items, ddof = 1), 2),'min': round(min(items), 2),
-                'max': round(max(items), 2)}
+    # using numpy library to calculate mean, median, min, max, var and std        
+    return {'mean': round(np.mean(items), 2),  'median': round(np.median(items), 2),
+            'var': round(np.var(items, ddof = 1), 2), 
+            'std': round(np.std(items, ddof = 1), 2),'min': round(min(items), 2),
+            'max': round(max(items), 2)}
         
 
 
