ce50290fe884c6b87a77a9d17d3183c11598c2ea
Cindy <60347554+RhulaniC@users.noreply.github.com>
2020-02-18 10:08:50 +0200

    Create test.py
    Testing to see if functions work.

---
+++ Tests/test.py
@@ -0,0 +1,13 @@
+from TeamPowerPackage import Module
+
+def TestFunctions():
+
+    assert Module.dictionary_of_metrics(gauteng) == {'mean': 26244.42,
+                                   'median': 24403.5,
+                                   'var': 108160153.17,
+                                   'std': 10400.01,
+                                   'min': 8842.0,
+                                   'max': 39660.0}
+
+
+    
