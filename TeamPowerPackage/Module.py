b4d06ed3abe85d331c041b0fd44064e999e6d984
Lebogang <lebogang.joyce@gmail.com>
2020-02-18 14:47:31 +0200

    Update Module.py
    Changed list name from a to b

--- TeamPowerPackage/Module.py
+++ TeamPowerPackage/Module.py
@@ -49,12 +49,12 @@ def five_num_summary(items):
         of the values as keys.
     """
     # your code here
-    a = 'max median min q1 q3'.split()
+    key = 'max median min q1 q3'.split()
     b = [round(np.max(items),2),round(np.median(items),2),
          round(np.min(items),2),round(np.quantile(items,0.25),2),
          round(np.quantile(items, 0.75),2)]
    
-    return dict( list(zip(a,b)))
+    return dict( list(zip(key,b)))
 
 
 
